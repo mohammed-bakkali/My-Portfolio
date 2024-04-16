103616692c3400c417f24c7560cebc3d7bf95187
Mohammed Bakkali <94966939+mohammed-bakkali@users.noreply.github.com>
2024-04-16 13:18:22 +0200

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -111,6 +111,7 @@ const filterCards = (e) => {
     }
   });
 };
+
 // Add click event listener to each filter
 filterButtons.forEach((button) =>
   button.addEventListener("click", filterCards)
@@ -118,7 +119,7 @@ filterButtons.forEach((button) =>
 
 // End  top Scroll
 
-/*~~~~~~~~~~~~~~~ NENU BUTTON ~~~~~~~~~~~~~~~*/
+/*~~~~~~~~~~~~~~~ MENU BUTTON ~~~~~~~~~~~~~~~*/
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".main-nav");
 const cancelBtn = document.querySelector(".cancel-btn");
@@ -193,11 +194,70 @@ document.addEventListener("click", function (e) {
   }
 });
 
+/*~~~~~~~~~~~~~~~ START SLAIDER REVIEWS ~~~~~~~~~~~~~~~*/
+// Select the carousel container element
+const carousel = document.querySelector(".carousel");
+
+// Select all arrow buttons within the container
+const arrowBtns = document.querySelectorAll(".container i");
+
+// Determine the width of the first card in the carousel
+const firstCardWidth = carousel.querySelector(".box").offsetWidth;
+
+// isDragging: A boolean flag to track whether the mouse button is being held down.
+let isDragging = false,
+  startX,
+  startScrollLeft;
+
+// Add event listeners for the arrow buttons to scroll the carousel left and right
+arrowBtns.forEach((arrowButton) => {
+  arrowButton.addEventListener("click", function () {
+    // Determine the direction of scrolling based on the arrow button clicked
+    if (arrowButton.id === "left") {
+      // If left arrow button clicked, scroll left by subtracting the width of the first card
+      carousel.scrollLeft -= firstCardWidth;
+    } else {
+      // If right arrow button clicked, scroll right by adding the width of the first card
+      carousel.scrollLeft += firstCardWidth;
+    }
+  });
+});
+
+// Event handler for when mouse drag starts
+const dragStart = (e) => {
+  isDragging = true;
+  carousel.classList.add("dragging");
+  // Records the initial cursor position and scroll position of the carousel
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+};
+
+// Event handler for mouse dragging
+const dragging = (e) => {
+  if (!isDragging) return; // If isDragging is false, return from here
+  // Update the scroll position of the carousel based on the cursor movement
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
+
+// Event handler for when mouse drag stops
+const dragStop = (e) => {
+  isDragging = false;
+  carousel.classList.remove("dragging");
+};
+
+// Add event listeners for mouse events
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+
+
+/*~~~~~~~~~~~~~~~ END SLAIDER REVIEWS ~~~~~~~~~~~~~~~*/
+
 /*~~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~*/
 const sr = ScrollReveal({
   origin: "top",
   distance: "60px",
-  duration: 2500,
+  duration: 1500,
   delay: 400,
 });
 
@@ -218,3 +278,5 @@ sr.reveal(".contact-text", { origin: "left" });
 sr.reveal(".contact-from", { origin: "lright" });
 
 sr.reveal(".footer");
+
+/*~~~~~~~~~~~~~~~  ENdSCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~*/
