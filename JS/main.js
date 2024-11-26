a3511a78c201de7c12dc9f2cf6b921194f146596
Mohammed Bakkali <94966939+mohammed-bakkali@users.noreply.github.com>
2024-11-26 02:22:24 +0100

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -267,32 +267,32 @@ else {
   distanceValue = "60px";
 }
 
-// const sr = ScrollReveal({
-//   origin: "top",
-//   distance: distanceValue,
-//   duration: 1500,
-//   delay: 400,
-// });
+const sr = ScrollReveal({
+  origin: "top",
+  distance: distanceValue,
+  duration: 1500,
+  delay: 400,
+});
 
-// sr.reveal(".home__image");
+sr.reveal(".home__image");
 
-// sr.reveal(".text", { origin: "bottom" });
+sr.reveal(".text", { origin: "bottom" });
 
-// sr.reveal(".category__box", { interval: 300 });
+sr.reveal(".category__box", { interval: 300 });
 
-// sr.reveal(".projects-container", { origin: "left" });
+sr.reveal(".projects-container", { origin: "left" });
 
-// sr.reveal(".about__img", { origin: "bottom" });
+sr.reveal(".about__img", { origin: "bottom" });
 
-// sr.reveal(".about__content", { origin: "top" });
+sr.reveal(".about__content", { origin: "top" });
 
-// sr.reveal(".customer__review", { origin: "right" });
+sr.reveal(".customer__review", { origin: "right" });
 
-// sr.reveal(".articles__boxes", { origin: "left" });
+sr.reveal(".articles__boxes", { origin: "left" });
 
-// sr.reveal(".contact-text", { origin: "left" });
+sr.reveal(".contact-text", { origin: "left" });
 
-// sr.reveal(".contact-form", { origin: "right" });
-// sr.reveal(".footer");
+sr.reveal(".contact-form", { origin: "right" });
+sr.reveal(".footer");
 
 /*~~~~~~~~~~~~~~~  ENdSCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~*/
